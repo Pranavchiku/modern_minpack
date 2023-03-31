ec5aa48e01c376bbaadd20ca91d915f8c8d2b658
Pranavchiku <goswami.4@iitj.ac.in>
2023-03-31 14:00:22 +0530

    make lfortran_run.sh generic

--- lfortran_run.sh
+++ lfortran_run.sh
@@ -1,2 +1,5 @@
+echo "Enter the name of the file: " 
+read name
+
 lfortran --implicit-typing --implicit-interface ./src/minpack.f90 -c
-lfortran --implicit-typing --implicit-interface ./examples/example_lmdif1.f90
+lfortran --implicit-typing --implicit-interface ./examples/$name.f90
